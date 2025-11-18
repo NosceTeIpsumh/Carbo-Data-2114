caabdb144764dedbb92d3b747ce51a6fd27fb091
thibault dupuis <dupuis.thib81@gmail.com>
2025-11-18 16:44:30 +0100

    recipes sans front

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,25 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_recipe, only: [:show]
+
   def index
+    @recipes = Recipe.all
   end
 
   def show
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = recipe.new(recipe_params)
+    @user = current_user
+    if @recipe.save!
+      redirect_to recipes_path
+    else
+      render :new, unprocessable_entity
   end
 
   def edit
@@ -18,5 +29,16 @@ class RecipesController < ApplicationController
   end
 
   def destroy
+  end
+end
+
+private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :steps, :difficulty, :indice_gly, :ratio_glucide, :user_id)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
