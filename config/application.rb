7a1b1df9ceaf8d30b3e64aa7eba68ad0746bbb35
martin dussenneMartin Dussenne <martin.dussenne@gmail.com>
2025-11-26 17:28:23 +0100

    fixes or not

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module CarboData2114
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :solid_queue
   end
 end
